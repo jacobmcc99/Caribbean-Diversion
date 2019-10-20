333c3c8724b0575f8f782f8cef78cf8f64fc693d
Abdiyee <abdiidris14@gmail.com>
2019-10-20 23:31:18 +0100

    setup

---
+++ gameproject/desktop/src/com/elevenfiftyfive/caribbeandiversion/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.elevenfiftyfive.caribbeandiversion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.elevenfiftyfive.caribbeandiversion.GDXGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.foregroundFPS = 60;
+		new LwjglApplication(new GDXGame(), config);
+	}
+}
